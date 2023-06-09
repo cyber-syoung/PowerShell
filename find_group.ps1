e6dc5128b006039901a972ca130fbebb927c3e10
Samuel Young <samyoungftw@gmail.com>
2023-06-09 14:43:23 -0400

    Added find_group
    Purpose of this script is to find all users in the group.

---
+++ find_group.ps1
@@ -0,0 +1,32 @@
+# Specify the OU path and group name
+$ouPath = "OU=DAY Users,OU=Dayton,DC=sugar-creek,DC=com"  # Replace with the desired OU path
+$groupName = "NET_VPN_EMPLOYEE"  # Replace with the desired group name
+
+# Connect to the Active Directory module
+Import-Module ActiveDirectory
+
+# Get the users in the specified OU
+$users = Get-ADUser -Filter {(EmployeeID -ne "NA")} -SearchBase $ouPath
+
+# Initialize an array to store the results
+$results = @()
+
+# Iterate through each user and check if they belong to the specified group
+foreach ($user in $users) {
+    $groupMembership = Get-ADUser $user -Properties MemberOf |
+                      Select-Object -ExpandProperty MemberOf
+
+    if ($groupMembership -contains $groupName) {
+        $result = @{
+            Username = $user.SamAccountName
+            Group = $groupName
+        }
+        $results += $result
+    }
+}
+
+# Specify the output file path
+$outputFilePath = "C:\VPN_Users.txt"  # Replace with the desired output file path
+
+# Export the results to a text file
+$results | Export-Csv -Path $outputFilePath -NoTypeInformation
